cf6512f15fac1078b6d154facc44bece6fa81526
Kenneth Koski <kenneth.koski@canonical.com>
2019-03-27 15:35:26 -0500

    Update and regularize deploy scripts
    Makes them work with Juju 2.6, and regularizes some drift between the
    two of them.

--- scripts/deploy-aws.sh
+++ scripts/deploy-aws.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
+CONTROLLER=aws-kf-controller
+CLOUD=aws-kf-cloud
+MODEL=aws-kf-model
+AWS_MODEL=default
+START=$(date +%s.%N)
+
 # Convenience for destroying resources created by this script
 case $1 in
     --destroy)
-    juju kill-controller aws-us-east-1
+    juju kill-controller $CONTROLLER
     exit 0
     ;;
 esac
@@ -18,12 +24,8 @@ trap cleanup EXIT
 
 set -eux
 
-CLOUD=aws-kf-cloud
-MODEL=aws-kf-model
-START=$(date +%s.%N)
-
 # Set up Kubernetes cloud on AWS
-juju bootstrap aws/us-east-1
+juju bootstrap aws/us-east-1 $CONTROLLER
 # Uncomment --overlay argument to use GPU instances
 juju deploy cs:bundle/canonical-kubernetes # --overlay overlays/cdk-gpu.yml
 juju deploy cs:~containers/aws-integrator
@@ -32,14 +34,14 @@ juju add-relation aws-integrator kubernetes-master
 juju add-relation aws-integrator kubernetes-worker
 
 # Wait for cloud to finish booting up
-juju wait -e aws-us-east-1:default -w
+juju wait -e $CONTROLLER:$AWS_MODEL -w
 
 juju expose kubeapi-load-balancer
 
 # Copy details of cloud locally, and tell juju about it
-juju scp -m default kubernetes-master/0:~/config $KUBECONFIG
+juju scp -m $AWS_MODEL kubernetes-master/0:~/config $KUBECONFIG
 
-juju add-k8s $CLOUD
+juju add-k8s $CLOUD -c $CONTROLLER
 juju add-model $MODEL $CLOUD
 
 # Set up some storage for the new cloud, deploy Kubeflow, and wait for
@@ -48,10 +50,10 @@ kubectl --kubeconfig=$KUBECONFIG create -f storage/aws-ebs.yml
 juju create-storage-pool operator-storage kubernetes storage-class=juju-operator-storage storage-provisioner=kubernetes.io/aws-ebs parameters.type=gp2
 juju create-storage-pool k8s-ebs kubernetes storage-class=juju-ebs storage-provisioner=kubernetes.io/aws-ebs parameters.type=gp2
 juju deploy kubeflow
-juju wait -e aws-us-east-1:$MODEL -w
+juju wait -e $CONTROLLER:$MODEL -w
 
 # Expose the Ambassador reverse proxy and print out a success message
-PUB_IP=$(juju status -m default | grep "kubernetes-worker/0" | awk '{print $5}')
+PUB_IP=$(juju status -m $AWS_MODEL | grep "kubernetes-worker/0" | awk '{print $5}')
 PUB_ADDR="${PUB_IP}.xip.io"
 
 juju config kubeflow-ambassador juju-external-hostname=$PUB_ADDR
@@ -84,7 +86,7 @@ The TF Jobs dashboard is available at http://${PUB_ADDR}/tfjobs/ui/
 
 To tear down Kubeflow and associated infrastructure, run this command:
 
-    juju kill-controller aws-us-east-1
+    juju kill-controller $CONTROLLER
 
 For more information, see documentation at:
 
