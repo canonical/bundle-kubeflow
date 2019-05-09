62abcb9685aa52d116f7ad5817b79fa8386ce1f6
Kenneth Koski <kenneth.koski@canonical.com>
2019-05-09 09:47:18 -0500

    Add Pipelines Viewer charm

---
+++ charms/pipelines-viewer/reactive/pipelines_viewer.py
@@ -0,0 +1,70 @@
+from pathlib import Path
+
+import yaml
+from charms import layer
+from charms.reactive import set_flag, clear_flag, when, when_not, hookenv
+
+
+@when('charm.pipelines-viewer.started')
+def charm_ready():
+    layer.status.active('')
+
+
+@when(
+    'layer.docker-resource.oci-image.changed',
+    'config.changed',
+)
+def update_image():
+    clear_flag('charm.pipelines-viewer.started')
+
+
+@when('layer.docker-resource.oci-image.available')
+@when_not('charm.pipelines-viewer.started')
+def start_charm():
+    layer.status.maintenance('configuring container')
+
+    image_info = layer.docker_resource.get_info('oci-image')
+    service_name = hookenv.service_name()
+
+    crd = yaml.load(Path('files/crd-v1beta1.yaml').read_text())
+
+    port = hookenv.config('port')
+
+    layer.caas_base.pod_spec_set({
+        'service': {'annotations': {
+            'getambassador.io/config': yaml.dump_all([
+                {
+                    'apiVersion': 'ambassador/v0',
+                    'kind':  'Mapping',
+                    'name':  'pipelines-viewer',
+                    'prefix': '/data',
+                    'rewrite': '/data',
+                    'service': f'{service_name}:{port}',
+                    'use_websocket': True,
+                    'timeout_ms': 30000,
+                },
+            ]),
+        }},
+        'containers': [
+            {
+                'name': 'pipelines-viewer',
+                'imageDetails': {
+                    'imagePath': image_info.registry_path,
+                    'username': image_info.username,
+                    'password': image_info.password,
+                },
+                'ports': [
+                    {
+                        'name': 'ui',
+                        'containerPort': port,
+                    },
+                ],
+            },
+        ],
+        'customResourceDefinitions': {
+            crd['metadata']['name']: crd['spec'],
+        },
+    })
+
+    layer.status.maintenance('creating container')
+    set_flag('charm.pipelines-viewer.started')
