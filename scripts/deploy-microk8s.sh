5752a509120d6a0354c61c5c4626ab5204b0fb24
Kenneth Koski <kenneth.koski@canonical.com>
2019-05-09 09:52:35 -0500

    Update deploy scripts support local deploys
    So that CI can test exactly what's in git, as opposed to the charm store

--- scripts/deploy-microk8s.sh
+++ scripts/deploy-microk8s.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
-missing=$(for cmd in juju juju-wait microk8s.kubectl; do command -v $cmd > /dev/null || echo $cmd; done)
+CONTROLLER=${CONTROLLER:-uk8s}
+CLOUD=${CLOUD:-microk8s}
+MODEL=kubeflow
+CHANNEL=${CHANNEL:-stable}
+BUILD=${BUILD:-false}
+START=$(date +%s.%N)
+
+missing=$(for cmd in juju juju-wait microk8s.kubectl $(if ${BUILD}; then echo juju-bundle; fi); do command -v $cmd > /dev/null || echo $cmd; done)
 if [[ -n "$missing" ]]; then
     echo "Some dependencies were not found. Please install them with:"
     echo
@@ -11,21 +18,11 @@ if [[ -n "$missing" ]]; then
     exit 1
 fi
 
-CONTROLLER=${CONTROLLER:-microk8s-controller}
-CLOUD=${CLOUD:-microk8s-cloud}
-MODEL=${MODEL:-microk8s-model}
-CHANNEL=${CHANNEL:-stable}
-START=$(date +%s.%N)
-
 # Convenience for destroying resources created by this script
 case $1 in
     --destroy)
-    # Clean up resources. Kill the controller with both `kill-controller`
-    # and `unregister` since edge doesn't support deleting k8s-bootstrapped
-    # controllers yet.
-    juju kill-controller $CONTROLLER
-    microk8s.kubectl delete ns $MODEL
-    juju remove-cloud --local $CLOUD
+    # Clean up resources.
+    juju destroy-controller --destroy-all-models --destroy-storage $CONTROLLER
     exit 0
     ;;
 esac
@@ -34,19 +31,32 @@ set -eux
 
 # Set up juju and microk8s to play nicely together
 sudo microk8s.enable dns storage dashboard
-microk8s.config | juju add-k8s $CLOUD
+
+for i in {1..5}; do microk8s.status --wait-ready --timeout 60 && break || sleep 10; done
+
 juju bootstrap $CLOUD $CONTROLLER
 juju add-model $MODEL $CLOUD
 # Uncomment this line to present local disks into microk8s as Persistent Volumes
 # microk8s.kubectl create -f storage/local-storage.yml || true
 juju create-storage-pool operator-storage kubernetes storage-class=microk8s-hostpath
-juju deploy kubeflow --channel $CHANNEL
-juju wait -w
 
-juju config kubeflow-ambassador juju-external-hostname=localhost
-juju expose kubeflow-ambassador
+# Allow building local bundle.yaml, otherwise deploy from the charm store
+if [[ "$BUILD" = true ]] ; then
+    juju bundle deploy --build
+else
+    juju deploy kubeflow --channel $CHANNEL
+fi
 
-AMBASSADOR_IP=$(juju status | grep "kubeflow-ambassador " | awk '{print $8}')
+juju wait -vw
+
+# General Kubernetes setup
+find charms/*/files/sa.yaml | xargs -I[] bash -c "microk8s.kubectl create -n $MODEL -f [] || true"
+find charms/*/files/secret.yaml | xargs -I[] bash -c "microk8s.kubectl create -n $MODEL -f [] || true"
+
+juju config ambassador juju-external-hostname=localhost
+juju expose ambassador
+
+AMBASSADOR_IP=$(juju status | grep "ambassador " | awk '{print $8}')
 
 END=$(date +%s.%N)
 DT=$(echo "($END - $START)/1" | bc)
@@ -59,8 +69,7 @@ Run \`microk8s.kubectl proxy\` to be able to access the dashboard at
 
     http://localhost:8001/api/v1/namespaces/kube-system/services/https:kubernetes-dashboard:/proxy/#!/overview?namespace=$MODEL
 
-The JupyterHub dashboard is available at http://${AMBASSADOR_IP}/hub/
-The TF Jobs dashboard is available at http://${AMBASSADOR_IP}/tfjobs/ui/
+The central dashboard is available at http://${AMBASSADOR_IP}/
 
 To tear down Kubeflow and associated infrastructure, run this command:
 
