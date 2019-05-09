eee5b5d7b3a93e54647bf5ded92641ff6bf64633
Kenneth Koski <kenneth.koski@canonical.com>
2019-05-09 09:49:13 -0500

    Add Redis charm

---
+++ charms/redis-k8s/reactive/redis.py
@@ -0,0 +1,59 @@
+from charmhelpers.core import hookenv
+from charms.reactive import set_flag, clear_flag, endpoint_from_flag, when, when_not
+
+from charms import layer
+
+
+@when('charm.redis.started')
+def charm_ready():
+    layer.status.active('')
+
+
+@when(
+    'layer.docker-resource.oci-image.changed',
+    'config.changed',
+)
+def update_image():
+    clear_flag('charm.redis.started')
+
+
+@when('layer.docker-resource.oci-image.available')
+@when_not('charm.redis.started')
+def start_charm():
+    layer.status.maintenance('configuring container')
+
+    image_info = layer.docker_resource.get_info('oci-image')
+
+    port = hookenv.config('port')
+
+    layer.caas_base.pod_spec_set({
+        'containers': [
+            {
+                'name': 'redis',
+                'imageDetails': {
+                    'imagePath': image_info.registry_path,
+                    'username': image_info.username,
+                    'password': image_info.password,
+                },
+                'ports': [
+                    {
+                        'name': 'redis',
+                        'containerPort': port,
+                    },
+                ],
+            },
+        ],
+    })
+
+    layer.status.maintenance('creating container')
+    set_flag('charm.redis.started')
+
+
+@when('endpoint.db.joined')
+def send_relation_info():
+    client = endpoint_from_flag('endpoint.db.joined')
+    random_unit = client.all_joined_units[0]
+    client.configure(
+        host=random_unit.received['ingress-address'],
+        port=hookenv.config('port'),
+    )
