c3431c5ee7e8ecd7f576725e9259dcc2b3333af3
Kenneth Koski <kenneth.koski@canonical.com>
2019-07-09 15:38:58 -0500

    Add Katib charms
    Adds Katib charms

---
+++ charms/katib-manager/reactive/katib_manager.py
@@ -0,0 +1,56 @@
+from charmhelpers.core import hookenv
+from charms import layer
+from charms.reactive import set_flag, clear_flag, when, when_not
+
+
+@when('charm.katib-manager.started')
+def charm_ready():
+    layer.status.active('')
+
+
+@when('layer.docker-resource.oci-image.changed')
+def update_image():
+    clear_flag('charm.katib-manager.started')
+
+
+@when('layer.docker-resource.oci-image.available', 'mysql.connected')
+@when_not('charm.katib-manager.started')
+def start_charm(mysql):
+    layer.status.maintenance('configuring container')
+
+    image_info = layer.docker_resource.get_info('oci-image')
+
+    layer.caas_base.pod_spec_set(
+        {
+            'containers': [
+                {
+                    'name': 'katib-manager',
+                    'command': ["./katib-manager"],
+                    'imageDetails': {
+                        'imagePath': image_info.registry_path,
+                        'username': image_info.username,
+                        'password': image_info.password,
+                    },
+                    'ports': [{'name': 'probe', 'containerPort': 6789}],
+                    'config': {'MYSQL_ROOT_PASSWORD': hookenv.config('mysql-root-password')},
+                    'livenessProbe': {
+                        'exec': {'command': ["/bin/grpc_health_probe", "-addr=:6789"]},
+                        'initialDelaySeconds': 10,
+                    },
+                    'readinessProbe': {
+                        'exec': {'command': ["/bin/grpc_health_probe", "-addr=:6789"]},
+                        'initialDelaySeconds': 5,
+                    },
+                },
+                {
+                    'name': 'katib-manager-rest',
+                    'command': ["./katib-manager-rest"],
+                    'image': 'gcr.io/kubeflow-images-public/katib/v1alpha2/katib-manager-rest:v0.1.2-alpha-289-g14dad8b',
+                    'ports': [{'name': 'rest', 'containerPort': 80}],
+                },
+            ]
+        }
+    )
+
+    layer.status.maintenance('creating container')
+    set_flag('charm.katib-manager.started')
---
+++ charms/katib-ui/reactive/katib_ui.py
@@ -0,0 +1,59 @@
+import yaml
+from charms import layer
+from charms.reactive import set_flag, clear_flag, when, when_not
+from charmhelpers.core import hookenv
+
+
+@when('charm.katib-ui.started')
+def charm_ready():
+    layer.status.active('')
+
+
+@when('layer.docker-resource.oci-image.changed')
+def update_image():
+    clear_flag('charm.katib-ui.started')
+
+
+@when('layer.docker-resource.oci-image.available')
+@when_not('charm.katib-ui.started')
+def start_charm():
+    layer.status.maintenance('configuring container')
+
+    image_info = layer.docker_resource.get_info('oci-image')
+
+    layer.caas_base.pod_spec_set(
+        {
+            'service': {
+                'annotations': {
+                    'getambassador.io/config': yaml.dump_all(
+                        [
+                            {
+                                'apiVersion': 'ambassador/v0',
+                                'kind': 'Mapping',
+                                'name': 'katib-ui',
+                                'prefix': '/katib/',
+                                'rewrite': '/katib/',
+                                'service': f'{hookenv.service_name()}:80',
+                                'timeout_ms': 30000,
+                            }
+                        ]
+                    )
+                }
+            },
+            'containers': [
+                {
+                    'name': 'katib-ui',
+                    'command': ["./katib-ui"],
+                    'imageDetails': {
+                        'imagePath': image_info.registry_path,
+                        'username': image_info.username,
+                        'password': image_info.password,
+                    },
+                    'ports': [{'name': 'port', 'containerPort': 80}],
+                }
+            ],
+        }
+    )
+
+    layer.status.maintenance('creating container')
+    set_flag('charm.katib-ui.started')
