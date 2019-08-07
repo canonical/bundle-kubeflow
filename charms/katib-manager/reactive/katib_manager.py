323c833fa3a382cff3fcd838f8ac9a0dd5e729ce
Kenneth Koski <kenneth.koski@canonical.com>
2019-08-07 16:50:38 -0500

    Clean up katib-manager charm
    Parameterizes hardcoded ports and image

--- charms/katib-manager/reactive/katib_manager.py
+++ charms/katib-manager/reactive/katib_manager.py
@@ -13,14 +13,22 @@ def update_image():
     clear_flag('charm.started')
 
 
-@when('layer.docker-resource.oci-image.available', 'mysql.connected')
+@when(
+    'layer.docker-resource.manager-image.available',
+    'layer.docker-resource.restful-image.available',
+    'mysql.connected',
+)
 @when_not('charm.started')
 def start_charm():
     layer.status.maintenance('configuring container')
 
-    image_info = layer.docker_resource.get_info('oci-image')
+    manager_image = layer.docker_resource.get_info('manager-image')
+    restful_image = layer.docker_resource.get_info('restful-image')
 
     mysql = endpoint_from_name('mysql')
+
+    manager_port = hookenv.config('manager-port')
+    restful_port = hookenv.config('restful-port')
 
     layer.caas_base.pod_spec_set(
         {
@@ -29,26 +37,30 @@ def start_charm():
                     'name': 'katib-manager',
                     'command': ["./katib-manager"],
                     'imageDetails': {
-                        'imagePath': image_info.registry_path,
-                        'username': image_info.username,
-                        'password': image_info.password,
+                        'imagePath': manager_image.registry_path,
+                        'username': manager_image.username,
+                        'password': manager_image.password,
                     },
-                    'ports': [{'name': 'probe', 'containerPort': 6789}],
+                    'ports': [{'name': 'manager', 'containerPort': manager_port}],
                     'config': {'MYSQL_ROOT_PASSWORD': mysql.password()},
                     'livenessProbe': {
-                        'exec': {'command': ["/bin/grpc_health_probe", "-addr=:6789"]},
+                        'exec': {'command': ["/bin/grpc_health_probe", f"-addr=:{manager_port}"]},
                         'initialDelaySeconds': 10,
                     },
                     'readinessProbe': {
-                        'exec': {'command': ["/bin/grpc_health_probe", "-addr=:6789"]},
+                        'exec': {'command': ["/bin/grpc_health_probe", f"-addr=:{manager_port}"]},
                         'initialDelaySeconds': 5,
                     },
                 },
                 {
                     'name': 'katib-manager-rest',
                     'command': ["./katib-manager-rest"],
-                    'image': 'gcr.io/kubeflow-images-public/katib/v1alpha2/katib-manager-rest:v0.1.2-alpha-289-g14dad8b',
-                    'ports': [{'name': 'rest', 'containerPort': 80}],
+                    'imageDetails': {
+                        'imagePath': restful_image.registry_path,
+                        'username': restful_image.username,
+                        'password': restful_image.password,
+                    },
+                    'ports': [{'name': 'restful', 'containerPort': restful_port}],
                 },
             ]
         }
