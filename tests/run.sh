f4e525d846624979fe38bb9ebeef56e8e7e24c4a
Kenneth Koski <kenneth.koski@canonical.com>
2019-08-22 12:19:09 -0500

    Add MNIST example pipeline
    Adds example pipeline that trains CNN model on MNIST dataset, then
    serves it in a sidecar to check predictions against MNIST examples.
    
    Also adds in a wrapper script that handles port forwarding to pipelines
    api to bypass ambassador auth.

---
+++ tests/run.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Forwards ports directly to pipelines-api to avoid Ambassador auth,
+# and submits pipelines to test Kubeflow.
+
+# Kill port forwarding at script end
+trap 'pkill -f svc/pipelines-api' SIGINT SIGTERM EXIT
+
+set -eux
+
+# Create connection directly to pipelines api, to get around ambassador auth
+juju kubectl port-forward svc/pipelines-api 8888:8888 &
+
+# Wait for port forwarding to spin up
+(i=10; while ! curl localhost:8888 ; do ((--i)) || exit; sleep 1; done)
+
+# Run tests
+pytest -vvs
--- tests/test_pipelines.py
+++ tests/test_pipelines.py
@@ -1,53 +1,49 @@
-import time
-from datetime import datetime
-from tempfile import NamedTemporaryFile
+from typing import Callable
 
-from kfp.compiler import Compiler
+import pytest
+from kfp import Client
 
-from .pipelines.cowsay import sequential_pipeline
-from .utils import get_session, get_pub_addr
+from .pipelines.cowsay import cowsay_pipeline
+from .pipelines.mnist import mnist_pipeline
 
 
-def test_pipelines():
-    sess = get_session()
-    now = datetime.now().strftime("%Y%m%d%H%M%S")
-    endpoint = f'http://{get_pub_addr()}/pipeline/apis/v1beta1'
-    pipeline_name = f'cowsay-{now}'
-    run_name = f'Cow Say {now}'
-    run_description = f"Automated testing run of pipeline {pipeline_name}"
+COWSAY_PARAMS = [{"name": "url", "value": "https://helloacm.com/api/fortune/"}]
 
-    with NamedTemporaryFile('w+b', suffix='.tar.gz') as f:
-        Compiler().compile(sequential_pipeline, f.name)
-        pipeline = sess.post(
-            f'{endpoint}/pipelines/upload', files={'uploadfile': f}, params={'name': pipeline_name}
-        ).json()
 
-        assert pipeline['name'] == pipeline_name
-        assert pipeline['parameters'] == [
-            {"name": "url", "value": "https://helloacm.com/api/fortune/"}
-        ]
+MNIST_PARAMS = [
+    {'name': 'storage-endpoint', 'value': 'minio:9000'},
+    {
+        'name': 'test-images',
+        'value': 'https://people.canonical.com/~knkski/t10k-images-idx3-ubyte.gz',
+    },
+    {
+        'name': 'test-labels',
+        'value': 'https://people.canonical.com/~knkski/t10k-labels-idx1-ubyte.gz',
+    },
+    {'name': 'train-batch-size', 'value': '128'},
+    {'name': 'train-epochs', 'value': '2'},
+    {
+        'name': 'train-images',
+        'value': 'https://people.canonical.com/~knkski/train-images-idx3-ubyte.gz',
+    },
+    {
+        'name': 'train-labels',
+        'value': 'https://people.canonical.com/~knkski/train-labels-idx1-ubyte.gz',
+    },
+]
 
-        pl_run = sess.post(
-            f'{endpoint}/runs',
-            json={
-                "description": run_description,
-                "name": run_name,
-                "pipeline_spec": {
-                    "parameters": [{"name": "url", "value": "https://helloacm.com/api/fortune/"}],
-                    "pipeline_id": pipeline['id'],
-                },
-                "resource_references": [],
-            },
-        ).json()['run']
 
-        for _ in range(30):
-            try:
-                pl_run = sess.get(f'{endpoint}/runs/{pl_run["id"]}').json()['run']
-                assert pl_run['status'] == 'Succeeded'
-                break
-            except KeyError:
-                print("Status not found on API response, retrying...")
-                time.sleep(5)
-            except AssertionError as err:
-                print(err)
-                time.sleep(5)
+@pytest.mark.parametrize(
+    'name,params,fn',
+    [('mnist', MNIST_PARAMS, mnist_pipeline), ('cowsay', COWSAY_PARAMS, cowsay_pipeline)],
+)
+def test_pipelines(name: str, params: list, fn: Callable):
+    """Runs each pipeline that it's been parameterized for, and waits for it to succeed."""
+
+    client = Client('127.0.0.1:8888')
+    run = client.create_run_from_pipeline_func(
+        fn, arguments={p['name']: p['value'] for p in params}
+    )
+    completed = client.wait_for_run_completion(run.run_id, timeout=1200)
+    status = completed.to_dict()['run']['status']
+    assert status == 'Succeeded', f'Pipeline status is {status}'
