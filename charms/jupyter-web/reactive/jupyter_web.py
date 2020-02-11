fbe8485b6d7d30a43301a8a9deb0408441c3ff8e
Kenneth Koski <kenneth.koski@canonical.com>
2020-02-11 12:49:44 -0600

    Update jupyter-web charm to 0.6
    Upstream's 1.0 requires Istio, which we don't yet have charmed

--- charms/jupyter-web/reactive/jupyter_web.py
+++ charms/jupyter-web/reactive/jupyter_web.py
@@ -1,10 +1,11 @@
 from glob import glob
 from pathlib import Path
-
+import os
 import yaml
+
 from charmhelpers.core import hookenv
 from charms import layer
-from charms.reactive import hook, clear_flag, set_flag, when, when_any, when_not
+from charms.reactive import clear_flag, endpoint_from_name, hook, set_flag, when, when_any, when_not
 
 
 @hook('upgrade-charm')
@@ -22,7 +23,7 @@ def update_image():
     clear_flag('charm.started')
 
 
-@when('layer.docker-resource.oci-image.available')
+@when('layer.docker-resource.oci-image.available', 'kubeflow-profiles.available')
 @when_not('charm.started')
 def start_charm():
     layer.status.maintenance('configuring container')
@@ -31,6 +32,11 @@ def start_charm():
     service_name = hookenv.service_name()
 
     port = hookenv.config('port')
+
+    profiles = endpoint_from_name('kubeflow-profiles').services()[0]
+    profiles_host = profiles['service_name']
+    profiles_port = profiles['hosts'][0]['port']
+    model = os.environ['JUJU_MODEL_NAME']
 
     layer.caas_base.pod_spec_set(
         {
@@ -91,13 +97,18 @@ def start_charm():
             },
             'containers': [
                 {
-                    'name': 'jupyterhub',
+                    'name': 'jupyter-web',
                     'imageDetails': {
                         'imagePath': image_info.registry_path,
                         'username': image_info.username,
                         'password': image_info.password,
                     },
                     'ports': [{'name': 'http', 'containerPort': port}],
+                    'config': {
+                        'USERID_HEADER': 'kubeflow-userid',
+                        'USERID_PREFIX': '',
+                        'KFAM': f'{profiles_host}.{model}.svc.cluster.local:{profiles_port}',
+                    },
                     'files': [
                         {
                             'name': 'configs',
