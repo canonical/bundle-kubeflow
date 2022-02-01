fa2f4306b3771f30e50a44887cd664c33b993544
Andrew Scribner <ca.scribner+1@gmail.com>
2022-02-01 16:44:51 -0500

    fix selenium tests for notebook
    Tests previously broken because upstream changed the New Notebook page in a way that broke our tests (something we select changed names).  This is fixed here.
    
    Other changes:
    * use dex username/password set in Juju rather than hard coded ones
    * add logging messages for debugging

--- conftest.py
+++ conftest.py
@@ -1,5 +1,6 @@
 def pytest_addoption(parser):
     parser.addoption("--proxy", action="store", help="Proxy to use")
     parser.addoption("--url", action="store", help="Juju controller")
-    parser.addoption("--password", action="store", help="Juju model")
+    parser.addoption("--username", action="store", help="Dex username (email address)")
+    parser.addoption("--password", action="store", help="Dex password")
     parser.addoption("--headful", action="store_true", help="Juju model")
