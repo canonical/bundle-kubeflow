e13689afcd153069ed0ea90d4bebfff174338151
Maksim Beliaev <beliaev.m.s@gmail.com>
2023-04-18 12:23:55 +0200

    do not raise on error

--- tests-bundle/1.6/test_1dot6.py
+++ tests-bundle/1.6/test_1dot6.py
@@ -22,7 +22,7 @@ async def test_deploy_1dot6(ops_test: OpsTest, lightkube_client, deploy_cmd):
     await ops_test.model.wait_for_idle(
         status="active",
         raise_on_blocked=False,
-        raise_on_error=True,
+        raise_on_error=False,
         timeout=3000,
     )
     print("All applications are active")
