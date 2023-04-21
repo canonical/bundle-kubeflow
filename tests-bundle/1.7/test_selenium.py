63cc7a5a9b351890314a627341a7ce9dfe7492bb
Maksim Beliaev <beliaev.m.s@gmail.com>
2023-04-21 10:18:10 +0200

    add selenium check

---
+++ tests-bundle/1.7/test_selenium.py
@@ -0,0 +1,10 @@
+import pytest
+from selenium.webdriver.common.by import By
+
+
+class TestGetStartedTutorial:
+    @pytest.mark.selenium
+    def test_create_notebook(self, driver):
+        driver.get("https://quay.io/")
+        msg = driver.find_element(by=By.XPATH, value="/html/body/div[1]/nav/span/div/div[1]/div[2]/ul[3]/li[4]/a")
+        assert msg.text.lower() == "sign in"
