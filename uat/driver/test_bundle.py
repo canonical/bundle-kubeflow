40b986ec3518a088f4e2d2aa73ff86e91175801b
Phoevos Kalemkeris <phoevos.kalemkeris@canonical.com>
2023-07-27 17:36:55 +0300

    driver: Add mechanism for automated UATs
    Add a driver to allow running the UATs included in the 'integrations'
    directory in an automated manner.
    
    Signed-off-by: Phoevos Kalemkeris <phoevos.kalemkeris@canonical.com>

---
+++ uat/driver/test_bundle.py
@@ -0,0 +1,110 @@
+# Copyright 2023 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+import logging
+import os
+from pathlib import Path
+
+import pytest
+from lightkube import ApiError, Client, codecs
+from lightkube.generic_resource import create_global_resource, load_in_cluster_generic_resources
+from utils import assert_namespace_active, delete_job, fetch_job_logs, wait_for_job
+
+log = logging.getLogger(__name__)
+
+ASSETS_DIR = Path("assets")
+JOB_TEMPLATE_FILE = ASSETS_DIR / "test-job.yaml.j2"
+PROFILE_TEMPLATE_FILE = ASSETS_DIR / "test-profile.yaml.j2"
+
+TESTS_DIR = os.path.abspath(Path("integrations"))
+TESTS_IMAGE = "kubeflownotebookswg/jupyter-scipy:v1.7.0"
+
+NAMESPACE = "test"
+PROFILE_RESOURCE = create_global_resource(
+    group="kubeflow.org",
+    version="v1",
+    kind="profile",
+    plural="profiles",
+)
+
+JOB_NAME = "test-kubeflow"
+
+
+@pytest.fixture(scope="module")
+def lightkube_client():
+    """Initialise Lightkube Client."""
+    lightkube_client = Client()
+    load_in_cluster_generic_resources(lightkube_client)
+    return lightkube_client
+
+
+@pytest.fixture(scope="module")
+def create_profile(lightkube_client):
+    """Create Profile and handle cleanup at the end of the module tests."""
+    log.info(f"Creating Profile {NAMESPACE}...")
+    resources = list(
+        codecs.load_all_yaml(
+            PROFILE_TEMPLATE_FILE.read_text(),
+            context={"namespace": NAMESPACE},
+        )
+    )
+    assert len(resources) == 1, f"Expected 1 Profile, got {len(resources)}!"
+    lightkube_client.create(resources[0])
+
+    yield
+
+    # delete the Profile at the end of the module tests
+    log.info(f"Deleting Profile {NAMESPACE}...")
+    lightkube_client.delete(PROFILE_RESOURCE, name=NAMESPACE)
+
+
+@pytest.mark.abort_on_fail
+async def test_create_profile(lightkube_client, create_profile):
+    """Test Profile creation.
+
+    This test relies on the create_profile fixture, which handles the Profile creation and
+    is responsible for cleaning up at the end.
+    """
+    try:
+        profile_created = lightkube_client.get(
+            PROFILE_RESOURCE,
+            name=NAMESPACE,
+        )
+    except ApiError as e:
+        if e.status == 404:
+            profile_created = False
+        else:
+            raise
+    assert profile_created, f"Profile {NAMESPACE} not found!"
+
+    assert_namespace_active(lightkube_client, NAMESPACE)
+
+
+def test_kubeflow(lightkube_client):
+    """Run a K8s Job to execute the notebook tests."""
+    log.info(f"Starting Kubernetes Job {NAMESPACE}/{JOB_NAME} to run notebook tests...")
+    resources = list(
+        codecs.load_all_yaml(
+            JOB_TEMPLATE_FILE.read_text(),
+            context={"job_name": JOB_NAME, "test_dir": TESTS_DIR, "test_image": TESTS_IMAGE},
+        )
+    )
+    assert len(resources) == 1, f"Expected 1 Job, got {len(resources)}!"
+    lightkube_client.create(resources[0], namespace=NAMESPACE)
+
+    try:
+        wait_for_job(lightkube_client, JOB_NAME, NAMESPACE)
+    except ValueError:
+        pytest.fail(
+            f"Something went wrong while running Job {NAMESPACE}/{JOB_NAME}. Please inspect the"
+            " attached logs for more info..."
+        )
+    finally:
+        log.info("Fetching Job logs...")
+        fetch_job_logs(JOB_NAME, NAMESPACE)
+
+
+def teardown_module():
+    """Cleanup resources."""
+    log.info(f"Deleting Job {NAMESPACE}/{JOB_NAME}...")
+    delete_job(JOB_NAME, NAMESPACE)
---
+++ uat/driver/utils.py
@@ -0,0 +1,81 @@
+# Copyright 2023 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+import logging
+import subprocess
+
+import tenacity
+from lightkube import Client
+from lightkube.resources.batch_v1 import Job
+from lightkube.resources.core_v1 import Namespace
+
+log = logging.getLogger(__name__)
+
+
+@tenacity.retry(
+    wait=tenacity.wait_exponential(multiplier=2, min=1, max=10),
+    stop=tenacity.stop_after_attempt(30),
+    reraise=True,
+)
+def assert_namespace_active(
+    client: Client,
+    namespace: str,
+):
+    """Test that the provided namespace is Active.
+
+    Retries multiple times to allow for the K8s namespace to be created and reach Active status.
+    """
+    # raises a 404 ApiError if the namespace doesn't exist
+    ns = client.get(Namespace, namespace)
+    phase = ns.status.phase
+
+    log.info(f"Waiting for namespace {namespace} to become 'Active': phase == {phase}")
+    assert phase == "Active", f"Waited too long for namespace {namespace}!"
+
+
+def _log_before_sleep(retry_state):
+    """Custom callback to log the number of seconds before the next attempt."""
+    next_attempt = retry_state.attempt_number
+    delay = retry_state.next_action.sleep
+    log.info(f"Retrying in {int(delay)} seconds (attempts: {next_attempt})")
+
+
+@tenacity.retry(
+    wait=tenacity.wait_exponential(multiplier=2, min=1, max=64),
+    retry=tenacity.retry_if_not_result(lambda result: result),
+    before_sleep=_log_before_sleep,
+    reraise=True,
+)
+def wait_for_job(
+    client: Client,
+    job_name: str,
+    namespace: str,
+):
+    """Wait for a Kubernetes Job to complete."""
+    # raises a 404 ApiError if the Job doesn't exist
+    job = client.get(Job, name=job_name, namespace=namespace)
+    if job.status.succeeded:
+        # stop retrying, Job succeeded
+        log.info(f"Job {namespace}/{job_name} completed successfully!")
+        return True
+    elif job.status.failed:
+        raise ValueError(f"Job {namespace}/{job_name} failed!")
+    elif not job.status.ready or job.status.active:
+        # continue retrying
+        status = "active" if job.status.active else "not ready"
+        log.info(f"Waiting for Job {namespace}/{job_name} to complete (status == {status})")
+        return False
+    else:
+        raise ValueError(f"Unknown status {job.status} for Job {namespace}/{job_name}!")
+
+
+def fetch_job_logs(job_name, namespace):
+    """Fetch the logs produced by a Kubernetes Job."""
+    command = ["kubectl", "logs", "-n", namespace, f"job/{job_name}"]
+    subprocess.check_call(command)
+
+
+def delete_job(job_name, namespace, lightkube_client=None):
+    """Delete a Kubernetes Job."""
+    client = lightkube_client or Client()
+    client.delete(Job, name=job_name, namespace=namespace)
