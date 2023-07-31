bfe6f0e53ab49a15f0d5c62e2e29e0831b77a360
Phoevos Kalemkeris <phoevos.kalemkeris@canonical.com>
2023-07-31 19:05:54 +0300

    retry: Add a timeout of 3600sec for the K8s Job
    Signed-off-by: Phoevos Kalemkeris <phoevos.kalemkeris@canonical.com>

--- uat/driver/utils.py
+++ uat/driver/utils.py
@@ -43,6 +43,7 @@ def _log_before_sleep(retry_state):
 @tenacity.retry(
     wait=tenacity.wait_exponential(multiplier=2, min=1, max=32),
     retry=tenacity.retry_if_not_result(lambda result: result),
+    stop=tenacity.stop_after_delay(60 * 60),
     before_sleep=_log_before_sleep,
     reraise=True,
 )
