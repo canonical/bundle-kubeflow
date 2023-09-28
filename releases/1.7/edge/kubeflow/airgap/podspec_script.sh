289d386d2d344f062143ab00a5f89c27ec4f436c
Orfeas Kourkakis <orfeas.kourkakis@canonical.com>
2023-09-28 17:57:42 +0300

    Convert katib-controller config to one line in podspec_script.sh

--- releases/1.7/edge/kubeflow/airgap/podspec_script.sh
+++ releases/1.7/edge/kubeflow/airgap/podspec_script.sh
@@ -5,25 +5,7 @@ juju deploy ./argo-server_08faf05.charm --resource oci-image=172.17.0.2:5000/arg
 juju deploy ./jupyter-controller_a870440.charm --resource oci-image=172.17.0.2:5000/kubeflownotebookswg/notebook-controller:v1.7.0
 juju deploy ./katib-controller_564a127.charm \
     --resource oci-image=172.17.0.2:5000/kubeflowkatib/katib-controller:v0.15.0 \
-    --config custom_images='{"default_trial_template": "172.17.0.2:5000/kubeflowkatib/mxnet-mnist:v0.15.0", \
-        "early_stopping__medianstop": "172.17.0.2:5000/kubeflowkatib/earlystopping-medianstop:v0.15.0", \
-        "enas_cpu_template": "172.17.0.2:5000/kubeflowkatib/enas-cnn-cifar10-cpu:v0.15.0", \
-        "metrics_collector_sidecar__stdout": "172.17.0.2:5000/kubeflowkatib/file-metrics-collector:v0.15.0", \
-        "metrics_collector_sidecar__file": "172.17.0.2:5000/kubeflowkatib/file-metrics-collector:v0.15.0", \
-        "metrics_collector_sidecar__tensorflow_event": "172.17.0.2:5000/kubeflowkatib/tfevent-metrics-collector:v0.15.0", \
-        "pytorch_job_template__master": "172.17.0.2:5000/kubeflowkatib/pytorch-mnist-cpu:v0.15.0", \
-        "pytorch_job_template__worker": "172.17.0.2:5000/kubeflowkatib/pytorch-mnist-cpu:v0.15.0", \
-        "suggestion__random": "172.17.0.2:5000/kubeflowkatib/suggestion-hyperopt:v0.15.0", \
-        "suggestion__tpe": "172.17.0.2:5000/kubeflowkatib/suggestion-hyperopt:v0.15.0", \
-        "suggestion__grid": "172.17.0.2:5000/kubeflowkatib/suggestion-optuna:v0.15.0", \
-        "suggestion__hyperband": "172.17.0.2:5000/kubeflowkatib/suggestion-hyperband:v0.15.0", \
-        "suggestion__bayesianoptimization": "172.17.0.2:5000/kubeflowkatib/suggestion-skopt:v0.15.0", \
-        "suggestion__cmaes": "172.17.0.2:5000/kubeflowkatib/suggestion-goptuna:v0.15.0", \
-        "suggestion__sobol": "172.17.0.2:5000/kubeflowkatib/suggestion-goptuna:v0.15.0", \
-        "suggestion__multivariate_tpe": "172.17.0.2:5000/kubeflowkatib/suggestion-optuna:v0.15.0", \
-        "suggestion__enas": "172.17.0.2:5000/kubeflowkatib/suggestion-enas:v0.15.0", \
-        "suggestion__darts": "172.17.0.2:5000/kubeflowkatib/suggestion-darts:v0.15.0", \
-        "suggestion__pbt": "172.17.0.2:5000/kubeflowkatib/suggestion-pbt:v0.15.0", }'
+    --config custom_images='{"default_trial_template": "172.17.0.2:5000/kubeflowkatib/mxnet-mnist:v0.15.0", "early_stopping__medianstop": "172.17.0.2:5000/kubeflowkatib/earlystopping-medianstop:v0.15.0", "enas_cpu_template": "172.17.0.2:5000/kubeflowkatib/enas-cnn-cifar10-cpu:v0.15.0", "metrics_collector_sidecar__stdout": "172.17.0.2:5000/kubeflowkatib/file-metrics-collector:v0.15.0", "metrics_collector_sidecar__file": "172.17.0.2:5000/kubeflowkatib/file-metrics-collector:v0.15.0", "metrics_collector_sidecar__tensorflow_event": "172.17.0.2:5000/kubeflowkatib/tfevent-metrics-collector:v0.15.0", "pytorch_job_template__master": "172.17.0.2:5000/kubeflowkatib/pytorch-mnist-cpu:v0.15.0", "pytorch_job_template__worker": "172.17.0.2:5000/kubeflowkatib/pytorch-mnist-cpu:v0.15.0", "suggestion__random": "172.17.0.2:5000/kubeflowkatib/suggestion-hyperopt:v0.15.0", "suggestion__tpe": "172.17.0.2:5000/kubeflowkatib/suggestion-hyperopt:v0.15.0", "suggestion__grid": "172.17.0.2:5000/kubeflowkatib/suggestion-optuna:v0.15.0", "suggestion__hyperband": "172.17.0.2:5000/kubeflowkatib/suggestion-hyperband:v0.15.0", "suggestion__bayesianoptimization": "172.17.0.2:5000/kubeflowkatib/suggestion-skopt:v0.15.0", "suggestion__cmaes": "172.17.0.2:5000/kubeflowkatib/suggestion-goptuna:v0.15.0", "suggestion__sobol": "172.17.0.2:5000/kubeflowkatib/suggestion-goptuna:v0.15.0", "suggestion__multivariate_tpe": "172.17.0.2:5000/kubeflowkatib/suggestion-optuna:v0.15.0", "suggestion__enas": "172.17.0.2:5000/kubeflowkatib/suggestion-enas:v0.15.0", "suggestion__darts": "172.17.0.2:5000/kubeflowkatib/suggestion-darts:v0.15.0", "suggestion__pbt": "172.17.0.2:5000/kubeflowkatib/suggestion-pbt:v0.15.0", }'
 juju deploy ./kfp-persistence_1b2dc2e.charm --resource oci-image=172.17.0.2:5000/charmedkubeflow/persistenceagent:2.0.0-alpha.7_22.04_1
 juju deploy ./kfp-profile-controller_a050a69.charm --resource oci-image=172.17.0.2:5000/python:3.7
 juju deploy ./kfp-schedwf_15ed6ef.charm --resource oci-image=172.17.0.2:5000/charmedkubeflow/scheduledworkflow:2.0.0-alpha.7_22.04_1
