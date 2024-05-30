a9ddc18b614cea66ee96414aa830b74f154ac8e3
NohaIhab <noha.ihab@canonical.com>
2024-05-30 08:54:48 +0000

    fix: add -y to install python without prompt

--- tests/airgapped/setup/prerequisites.sh
+++ tests/airgapped/setup/prerequisites.sh
@@ -2,7 +2,7 @@ JUJU_CHANNEL="${JUJU_CHANNEL:-2.9/stable}"
 
 echo "Installing pip"
 sudo apt update
-sudo apt install python3-pip
+sudo apt -y install python3-pip
 
 echo "Installing Juju"
 sudo snap install juju --channel=$JUJU_CHANNEL --classic
