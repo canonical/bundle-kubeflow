b55ed3ff5a07db3daeda7255f1a7e10379528548
NohaIhab <noha.ihab@canonical.com>
2024-06-14 09:48:04 +0000

    feat: add image to get images script

--- scripts/airgapped/get-all-images.sh
+++ scripts/airgapped/get-all-images.sh
@@ -46,6 +46,9 @@ rm -rf resource-dispatcher
 # manually retrieve pipelines runner image to test pipelines
 IMAGES+=($(echo "charmedkubeflow/pipelines-runner:ckf-1.8"))
 
+# manually retrieve katib experiment image
+IMAGES+=($(echo "docker.io/kubeflowkatib/simple-pbt:v0.16.0"))
+
 # ensure we only show unique images
 IMAGES=($(echo "${IMAGES[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
 
