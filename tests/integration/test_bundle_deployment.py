baa9cc9ee798b928f6420864c18706d1002f7689
Daniela Plascencia <daniela.plascencia@canonical.com>
2024-07-31 13:11:39 +0200

    fix, tests: add condition for waiting for and setting the public-url config option
    Due to recent changes in dex and oidc charms for CKF 1.9, this configuration option is not
    required anymore for this and greater versions of the bundle.
    The config option is still required by CKF 1.8 charms, though, to allow this test to run for
    1.8 and 1.9, this commits adds logic to set it or not depending on the deployed version.
    
    Fixes #997

--- tests/integration/test_bundle_deployment.py
+++ tests/integration/test_bundle_deployment.py
@@ -10,15 +10,19 @@ from pytest_operator.plugin import OpsTest
 from lightkube.resources.core_v1 import Service
 
 BUNDLE_NAME = "kubeflow"
+BUNDLE_VERSION = os.environ.get("BUNDLE_VERSION")
+
 
 @pytest.fixture()
 def lightkube_client() -> lightkube.Client:
     client = lightkube.Client(field_manager=BUNDLE_NAME)
     return client
 
+
 @pytest.fixture
 def bundle_path() -> str:
-    return os.environ.get("BUNDLE_PATH").replace("\"", "")
+    return f"./releases/{BUNDLE_VERSION}/stable/kubeflow/bundle.yaml"
+
 
 class TestCharm:
     @pytest.mark.abort_on_fail
@@ -33,17 +37,24 @@ class TestCharm:
             timeout=1500,
         )
 
-        await ops_test.model.wait_for_idle(
-            apps=["oidc-gatekeeper"],
-            status="blocked",
-            raise_on_blocked=False,
-            raise_on_error=False,
-            timeout=1500,
-        )
-
-        url = get_public_url(lightkube_client, BUNDLE_NAME)
-        await ops_test.model.applications["dex-auth"].set_config({"public-url": url})
-        await ops_test.model.applications["oidc-gatekeeper"].set_config({"public-url": url})
+        # To keep compatibility with CKF 1.8, the public-url configuration
+        # must be set. For >=1.9 this is not required.
+        # TODO: remove when CKF 1.8 falls out of support
+        if BUNDLE_VERSION == "1.8":
+            await ops_test.model.wait_for_idle(
+                apps=["oidc-gatekeeper"],
+                status="blocked",
+                raise_on_blocked=False,
+                raise_on_error=False,
+                timeout=1500,
+            )
+            url = get_public_url(lightkube_client, BUNDLE_NAME)
+            await ops_test.model.applications["dex-auth"].set_config({"public-url": url})
+            await ops_test.model.applications["oidc-gatekeeper"].set_config({"public-url": url})
+        else:
+            await ops_test.model.add_relation(
+                "oidc-gatekeeper:dex-oidc-config", f"dex-auth:dex-oidc-config"
+            )
 
         await ops_test.model.wait_for_idle(
             status="active",
@@ -58,6 +69,7 @@ class TestCharm:
         assert "Email Address" in result_text
         assert "Password" in result_text
 
+
 def get_public_url(lightkube_client: lightkube.Client, bundle_name: str):
     """Extracts public url from service istio-ingressgateway-workload for EKS deployment.
     As a next step, this could be generalized in order for the above test to run in MicroK8s as well.
@@ -65,9 +77,13 @@ def get_public_url(lightkube_client: lightkube.Client, bundle_name: str):
     ingressgateway_svc = lightkube_client.get(
         Service, "istio-ingressgateway-workload", namespace=bundle_name
     )
-    address = ingressgateway_svc.status.loadBalancer.ingress[0].hostname or ingressgateway_svc.status.loadBalancer.ingress[0].ip
+    address = (
+        ingressgateway_svc.status.loadBalancer.ingress[0].hostname
+        or ingressgateway_svc.status.loadBalancer.ingress[0].ip
+    )
     public_url = f"http://{address}"
     return public_url
+
 
 async def fetch_response(url, headers=None):
     """Fetch provided URL and return pair - status and text (int, string)."""
