6278e1dc93431d640125531f1fcb16b36bfeca07
Daniela Plascencia <daniela.plascencia@canonical.com>
2024-08-06 12:05:52 +0200

    skip: keep old format as we'll get rid of it soon with sqa tests anyway

--- tests/integration/test_bundle_deployment.py
+++ tests/integration/test_bundle_deployment.py
@@ -9,8 +9,6 @@ import requests
 from pytest_operator.plugin import OpsTest
 from lightkube.resources.core_v1 import Service
 
-from _pytest.config.argparsing import Parser
-
 BUNDLE_NAME = "kubeflow"
 
 @pytest.fixture()
@@ -18,32 +16,30 @@ def lightkube_client() -> lightkube.Client:
     client = lightkube.Client(field_manager=BUNDLE_NAME)
     return client
 
-
 @pytest.fixture
-def bundle_path(request) -> str:
-    """Return the relative path to the bundle file given a track and risk."""
-    track = request.config.getoption("track")
-    risk = request.config.getoption("risk")
-    bundle_path = sh.find("./releases/{track}/{risk}", "-name", "bundle.yaml")
-    return bundle_path
-
+def bundle_path() -> str:
+    return os.environ.get("BUNDLE_PATH").replace("\"", "")
 
 class TestCharm:
     @pytest.mark.abort_on_fail
-    async def test_bundle_deployment_works(self, ops_test: OpsTest, lightkube_client, bundle_path, request):
+    async def test_bundle_deployment_works(self, ops_test: OpsTest, lightkube_client, bundle_path):
         subprocess.Popen(["juju", "deploy", bundle_path, "--trust"])
+
+        # Wait for istio-ingressgateway charm to be active and idle
+        # This is required because later we'll try to fetch a response from the login url
+        # using the ingress gateway service IP address (provided by the LoadBalancer)
+        await ops_test.model.wait_for_idle(
+            apps=["istio-ingressgateway"],
+            status="active",
+            raise_on_blocked=False,
+            raise_on_error=False,
+            timeout=1500,
+        )
+
         # To keep compatibility with CKF 1.8, the public-url configuration
         # must be set. For >=1.9 this is not required.
         # TODO: remove when CKF 1.8 falls out of support
-        if request.config.getoption("track") == "1.8":
-            await ops_test.model.wait_for_idle(
-                apps=["istio-ingressgateway"],
-                status="active",
-                raise_on_blocked=False,
-                raise_on_error=False,
-                timeout=1500,
-            )
-
+        if "1.8" in bundle_path:
             await ops_test.model.wait_for_idle(
                 apps=["oidc-gatekeeper"],
                 status="blocked",
@@ -51,7 +47,7 @@ class TestCharm:
                 raise_on_error=False,
                 timeout=1500,
             )
-            url = get_public_url(lightkube_client, BUNDLE_NAME)
+    
             await ops_test.model.applications["dex-auth"].set_config({"public-url": url})
             await ops_test.model.applications["oidc-gatekeeper"].set_config({"public-url": url})
         else:
@@ -59,6 +55,7 @@ class TestCharm:
                 "oidc-gatekeeper:dex-oidc-config", f"dex-auth:dex-oidc-config"
             )
 
+        # Wait for the whole bundle to become active and idle
         await ops_test.model.wait_for_idle(
             status="active",
             raise_on_blocked=False,
@@ -66,12 +63,12 @@ class TestCharm:
             timeout=1500,
         )
 
+        url = get_public_url(lightkube_client, BUNDLE_NAME)
         result_status, result_text = await fetch_response(url)
         assert result_status == 200
         assert "Log in to Your Account" in result_text
         assert "Email Address" in result_text
         assert "Password" in result_text
-
 
 def get_public_url(lightkube_client: lightkube.Client, bundle_name: str):
     """Extracts public url from service istio-ingressgateway-workload for EKS deployment.
@@ -80,13 +77,9 @@ def get_public_url(lightkube_client: lightkube.Client, bundle_name: str):
     ingressgateway_svc = lightkube_client.get(
         Service, "istio-ingressgateway-workload", namespace=bundle_name
     )
-    address = (
-        ingressgateway_svc.status.loadBalancer.ingress[0].hostname
-        or ingressgateway_svc.status.loadBalancer.ingress[0].ip
-    )
+    address = ingressgateway_svc.status.loadBalancer.ingress[0].hostname or ingressgateway_svc.status.loadBalancer.ingress[0].ip
     public_url = f"http://{address}"
     return public_url
-
 
 async def fetch_response(url, headers=None):
     """Fetch provided URL and return pair - status and text (int, string)."""
@@ -97,20 +90,3 @@ async def fetch_response(url, headers=None):
             result_status = response.status
             result_text = await response.text()
     return result_status, str(result_text)
-
-
-def pytest_addoption(parser: Parser):
-    parser.addoption(
-        "--track",
-        default="1.9",
-        help="Charmed Kubeflow bundle track where the version to be tested is."
-        "Defaults to the latest patch version."
-        "Allowed values: latest, 1.8, 1.9",
-    )
-    parser.addoption(
-        "--risk",
-        default="stable"
-        help="The risk level from the Charmhub track."
-        "Defaults to stable."
-        "Allowed values: stable, edge, beta.",
-    )
