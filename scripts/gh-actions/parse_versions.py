5079d6375c2eb46b09daf7e708a79899f00a53fc
mvlassis <manos.vlassis@canonical.com>
2024-09-04 13:09:26 +0300

    Update deploy-to-eks.yaml with the Python script

--- scripts/gh-actions/parse_versions.py
+++ scripts/gh-actions/parse_versions.py
@@ -2,8 +2,9 @@ import os
 import sys
 import json
 
+# Parse the versions given as a comma-separated list and return a JSON array
 def parse_versions(input_versions):
-    # Default version string if input is empty
+    # Default version string if the input is empty
     if not input_versions:
         input_versions = "1.8,1.9,latest"
     else:
@@ -15,13 +16,10 @@ def parse_versions(input_versions):
     return json_array
 
 if __name__ == "__main__":
-    # Read the input version from the environment variable
+    # Read the input of the Github Action from the environment variable
     input_versions = os.getenv('INPUT_BUNDLE_VERSION', '')
-    # Parse the versions
     json_array = parse_versions(input_versions)
-    # Output the result to GitHub Actions
     print(f"bundle_versions={json_array}")
-    # Write to GitHub Actions output
     with open(os.environ['GITHUB_OUTPUT'], 'a') as output_file:
         output_file.write(f"bundle_versions={json_array}\n")
 
