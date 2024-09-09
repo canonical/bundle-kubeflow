0fadcf76b73fa6eaed383f9cf0ac8230358c821b
mvlassis <manos.vlassis@canonical.com>
2024-09-09 13:37:46 +0300

    Remove dependencies from setup.sh

--- tests/airgapped/setup/setup.sh
+++ tests/airgapped/setup/setup.sh
@@ -8,9 +8,5 @@ cat tests/airgapped/lxd.profile | lxd init --preseed
 ./tests/airgapped/setup/lxd-docker-networking.sh
 
 pip3 install -r ./scripts/requirements.txt
-sudo apt install pigz
-sudo snap install docker
-sudo snap install yq
-sudo snap install jq
 
 echo "Setup completed. Reboot your machine before running the tests for the docker commands to run without sudo."
